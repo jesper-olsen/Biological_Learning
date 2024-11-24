0e5acca71f55898f1af2f04937db7663a9ddc883
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2024-11-24 09:50:39 -0600

    rename, doc

--- Unsupervised_learning_algorithm_MNIST.py
+++ Unsupervised_learning_algorithm_MNIST.py
@@ -10,11 +10,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def draw_weights(ep, fig, synapses, GRID_X, GRID_Y):
+def draw_weights(ep, fig, weights, GRID_X, GRID_Y):
     HM = np.zeros((28 * GRID_Y, 28 * GRID_X))
     for y in range(GRID_Y):
         for x in range(GRID_X):
-            HM[y * 28 : (y + 1) * 28, x * 28 : (x + 1) * 28] = synapses[
+            HM[y * 28 : (y + 1) * 28, x * 28 : (x + 1) * 28] = weights[
                 y * GRID_X + x, :
             ].reshape(28, 28)
     plt.clf()
@@ -38,6 +38,10 @@ def load_mnist(file_path, normalize=True):
     return data
 
 
+def init_weights(num_hidden, input_dim, mean=0.0, std_dev=1.0):
+    return np.random.normal(mean, std_dev, (num_hidden, input_dim))
+
+
 def train():
     data = load_mnist("mnist_all.mat")
     IDIM = 784
@@ -49,44 +53,38 @@ def train():
     BATCH_SIZE = 100  # size of the minibatch
     EPSILON = 1e-30  # numerical precision of weight updates
     DELTA = 0.4  # Strength of the anti-hebbian learning
-    P = 2.0  # Lebesgue norm of the weights
+    LEBESGUE_NORM = 2
     RANK_PARAM = 2  #  must be integer that is bigger or equal than 2
-
     fig = plt.figure(figsize=(12.9, 10))
 
-    # For every minibatch the overlap with the data `tot_input` is calculated for  each data point and
-    # each hidden unit. The sorted strengths of the # activations are stored in `y`. The variable `yl`
-    # stores the activations of the post synaptic cells - it is denoted by g(Q) in Eq 3 of the paper.
-    # See also Eq 9 and Eq 10.
-    # The variable `ds` is the right hand side of Eq 3. The weights are updated after each minibatch
-    # in a way so that the largest update is equal to the learning rate at that epoch.
-    mu, sigma = 0.0, 1.0  # mean and standard deviation - for weight initialisation
-    synapses = np.random.normal(mu, sigma, (NHID, IDIM))
+    weights = init_weights(NHID, IDIM)
     for ep in range(MAX_EPOCHS):
         lr = LR0 * (1 - ep / MAX_EPOCHS)
         np.random.shuffle(data)
         for i in range(NTRAIN // BATCH_SIZE):
             batch = data[i * BATCH_SIZE : (i + 1) * BATCH_SIZE, :].T
-            sig = np.sign(synapses)
-            tot_input = np.dot(sig * np.absolute(synapses) ** (P - 1), batch)
+            signs = np.sign(weights)
+            tot_input = np.dot(signs * np.abs(weights) ** (LEBESGUE_NORM - 1), batch)
 
+            # y: activations sorted by strength
             y = np.argsort(tot_input, axis=0)  # 100x100
+            # yl: post synaptic activations - denoted g(Q) in the paper, see eq 3 + 9 & 10
             yl = np.zeros((NHID, BATCH_SIZE))  # 100x100
             yl[y[NHID - 1, :], np.arange(BATCH_SIZE)] = 1.0
             yl[y[NHID - RANK_PARAM], np.arange(BATCH_SIZE)] = -DELTA
-
-            xx = np.sum(np.multiply(yl, tot_input), 1)  # 100x1
-            # delta_synapses 100x784
-            delta_synapses = np.dot(yl, batch.T) - np.multiply(
-                np.tile(xx.reshape(xx.shape[0], 1), (1, IDIM)), synapses
+            activation_sums = np.sum(np.multiply(yl, tot_input), 1)  # 100x1
+            # delta_weights 100x784 - right hand side of Eq 3
+            delta_weights = np.dot(yl, batch.T) - np.multiply(
+                weights, activation_sums[:, None]
             )
 
-            norm_const = np.amax(np.absolute(delta_synapses))
+            # weight update - largest update is equal to the learning rate
+            norm_const = np.amax(np.absolute(delta_weights))
             norm_const = max(norm_const, EPSILON)
-            synapses += lr * np.true_divide(delta_synapses, norm_const)
+            weights += lr * np.true_divide(delta_weights, norm_const)
 
         print(f"Epoch {ep}; norm_const {norm_const}")
-        draw_weights(ep, fig, synapses, GRID_X, GRID_Y)
+        draw_weights(ep, fig, weights, GRID_X, GRID_Y)
     plt.show()  # pause until user kills the window
 
 
